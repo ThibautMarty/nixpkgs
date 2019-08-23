0c1c90db3ccf9dbcc55b221f0cd15053cd86ef62
Thibaut Marty <github@thibautmarty.fr>
2019-08-23 16:52:08 +0200

    hiredis: fix ar reference for cross compilation

--- pkgs/development/libraries/hiredis/default.nix
+++ pkgs/development/libraries/hiredis/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchFromGitHub, fetchpatch }:
 
 stdenv.mkDerivation rec {
   name = "hiredis-${version}";
@@ -10,6 +10,11 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     sha256 = "0ik38lwpmm780jqrry95ckf6flmvd172444p3q8d1k9n99jwij9c";
   };
+
+  patches = [ (fetchpatch {
+    url = https://github.com/redis/hiredis/commit/2fa34e37af0881ff3700fea64fe211bbb8b028a2.patch;
+    sha256 = "1qiy5k9i1j90y4lx4ya2kf7bk6f0nq684qsp002kp6hfzc5aj9g2";
+  }) ];
 
   PREFIX = "\${out}";
 
